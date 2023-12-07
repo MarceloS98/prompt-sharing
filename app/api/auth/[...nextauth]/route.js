b245df10f469245b6d4898cc2365c6ad426a6365
Marcelo Sosa <sosamarcelo92@gmail.com>
2023-12-07 20:55:56 -0300

    move session and signin functions to callbacks

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,36 +11,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      try {
+        const sessionUser = await User.findOne({ email: session.user.email });
+        session.user.id = sessionUser._id.toString();
 
-  async session({ session }) {
-    try {
-      const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = sessionUser._id.toString();
-
-      return session;
-    } catch (error) {
-      console.log(error);
-    }
-  },
-
-  async signIn({ profile }) {
-    try {
-      await connectToDatabase();
-
-      const userExist = await User.findOne({ email: profile.email });
-
-      if (!userExist) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        return session;
+      } catch (error) {
+        console.log(error);
       }
+    },
 
-      return true;
-    } catch (error) {
-      console.log(error);
-    }
+    async signIn({ profile }) {
+      try {
+        await connectToDatabase();
+
+        const userExist = await User.findOne({ email: profile.email });
+
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 });
 
